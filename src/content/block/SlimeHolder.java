7431eaf113b1174a032dfe247d4b8be518f31144
shluf <luthfisalis09@gmail.com>
2024-06-27 03:16:10 +0700

    Refactoring

--- src/content/block/SlimeHolder.java
+++ src/content/block/SlimeHolder.java
@@ -3,8 +3,9 @@ package content.block;
 import content.ObjectID;
 
 public class SlimeHolder extends Square {
+    private static final ObjectID type = ObjectID.SLIMEHOLDER;
 
     public SlimeHolder(float x, float y, float width, float height, int scale) {
-        super(x, y, ObjectID.SLIMEHOLDER, width, height, scale);
+        super(x, y, type, width, height, scale);
     }
 }
